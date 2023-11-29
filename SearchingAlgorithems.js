97800521cdfcda1714b405b610f02d9d464125f6
duressa@2022 <duressashukuri2022@gmail.com>
2023-11-29 06:16:04 -0800

    preorder Search is added

--- SearchingAlgorithems.js
+++ SearchingAlgorithems.js
@@ -1,3 +1,17 @@
+/*
+ * DO NOT ALTER OR REMOVE THIS  NOTICES OR THIS FILE HEADER FROM THE CODE.
+ * This code is free software you can redistribute it and/or modify it
+   published by the Good will of the author.
+
+ * This code is distributed in the hope that it will be useful,Therefore
+   use this code for your need or purpose and you can inform Error or part
+   to modify or add.
+ * Modifiying this code in a part or as Whole is possible and incremental modification is
+   suggested please inform the author any modification you have done.
+ * Please contact the Author,if you need additional information or have any questions.
+     *Author:Duressa Shukuri
+     *Email:duressashukuri2022@gmail.com
+*/
 function linearSearch(array,key){
     for(let i=0;i<array.length;i++){
         if(array[i]==key){
@@ -21,4 +35,16 @@ function binarySearch(array,key){
     }
     return -1
 }
-console.log(binarySearch([1,2,3,4,5,6,7,8,9],4))
+function preOrderSearch(array,root,key){
+    if(root<array.length){
+        if(array[root]==key){
+            return true
+        }
+        left=preOrderSearch(array,root*2+1,key)
+        right=preOrderSearch(array,root*2+2,key)
+        return left || right
+    }
+    return false
+
+}
+console.log(preOrderSearch([1,4,7,8,5,2,3],0,7))
