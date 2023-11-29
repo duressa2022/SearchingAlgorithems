07f3de081c58983eee9210f9273175dfb22a684e
duressa@2022 <duressashukuri2022@gmail.com>
2023-11-29 06:08:00 -0800

    Binary Search is added

--- SearchingAlgorithems.js
+++ SearchingAlgorithems.js
@@ -6,4 +6,19 @@ function linearSearch(array,key){
     }
     return -1
 }
-console.log(linearSearch([1,4,7,8,5,2,3,6],5))
+function binarySearch(array,key){
+    let low=0
+    let high=array.length-1
+    while(low<=high){
+        let mid=Math.floor((low+high)/2)
+        if(array[mid]==key){
+            return mid
+        }else if(array[mid]>key){
+            high=mid-1
+        }else{
+            low=mid+1
+        }
+    }
+    return -1
+}
+console.log(binarySearch([1,2,3,4,5,6,7,8,9],4))
